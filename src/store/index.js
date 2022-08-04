e3dcce80dab6428748141ae8b7837f44077fb17d
wangzhanjing <w15733570159@163.com>
2022-08-04 21:24:46 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,9 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
   },
+  getters: {
+
+  },
   mutations: {
   },
   actions: {
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    App
+
   </div>
 </template>
+
 <script>
 export default {
 
 }
 </script>
+
+<style>
+</style>
